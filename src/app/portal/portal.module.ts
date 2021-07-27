f404231fc080b4d2576823a425e471fac3475073
Timothy Meehan <tim.meehan097@gmail.com>
2021-07-27 15:56:39 +0700

    updated nguniversal/builder + added indications on search component

--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PortalComponent } from './portal.component';
-import { SearchComponent } from '../search/search.component';
 
 // === 1st: portal routes ===
 const routes: Routes = [
   {
     path: '',
-    // component: SearchComponent,
     loadChildren: () => import('../search/search.module').then(m => m.SearchModule)
   },
 ];
